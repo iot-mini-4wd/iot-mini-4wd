588920a8b02b0775705a5460ad51f669ee6db6de
Noriyoshi Kamado <noriyoshi.kamado.de@nttdocomo.com>
2016-10-23 17:27:59 +0900

    mini4wdライブラリをモジュール化するためのsetup.pyを追加

---
+++ setup.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+from setuptools import setup
+
+package_name="mini4wd"
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    description='Library to control the mini-4WD in the IoT',
+    packages=[package_name,],
+)
